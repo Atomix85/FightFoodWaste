36e20f62f173f70d3853e26760ffb475a714d1a9
Atomix85 <44714073+Atomix85@users.noreply.github.com>
2019-03-26 00:09:09 +0100

    curl test

--- CollectManager/client/src/utils/httpRequest.h
+++ CollectManager/client/src/utils/httpRequest.h
@@ -1,7 +1,13 @@
 #include <curl/curl.h>
 
 int userConnect(char* user, char* psw){
+    char* answer;
+    sendRequest("world.openfoodfacts.org/api/v0/product/737628064502.json",
+                1,"",answer);
 
+}
+
+int sendRequest(char* url, int method ,char* args, char* answer ){
     CURL * curl;
     CURLcode res;
 
@@ -11,17 +17,31 @@ int userConnect(char* user, char* psw){
     if(curl){
 
         //Specify URL and POST args
-        curl_easy_setopt(curl, CURLOPT_URL, "http://3dhandler.org");
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        if(method == 0){
+            //GET
+        }else
+            //POST
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, args);
 
         //Send request
         res = curl_easy_perform(curl);
 
-        if(res != CURLE_OK)
+        if(res != CURLE_OK){
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
+            curl_easy_cleanup(curl);
+            return 0;
+        }
+        else{
+            res = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE,answer);
+            if((res==CURLE_OK) && answer){
+                fprintf(stderr, "\n\n\n%s",
+              &answer);
+            }
+            curl_easy_cleanup(curl);
+            return 1;
+        }
 
-        curl_easy_cleanup(curl);
     }
-    return 1;
 }
